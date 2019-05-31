9f4ee9eed5548eb249a014135438a768dadbaf3b
kaiserawu <kaiser.a.wu@gmail.com>
2019-05-30 18:55:09 -0700

    Set up knex, db, and initial migration

---
+++ migrations/20190530182124_recipe_book.js
@@ -0,0 +1,34 @@
+
+exports.up = async function(knex) {
+  await knex.schema.createTable('dishes', tbl => {
+    tbl.increments('id');
+    tbl.string('name').unique().notNullable();
+  })
+
+  await knex.schema.createTable('recipes', tbl => {
+    tbl.increments('id');
+    tbl.string('name').unique().notNullable();
+    tbl.string('instructions').notNullable();
+    tbl.integer('dish_id').references('id').inTable('dishes').onDelete('CASCADE').onUpdate('CASCADE').notNullable();
+  })
+
+  await knex.schema.createTable('ingredients', tbl => {
+    tbl.increments('id');
+    tbl.string('name').unique().notNullable();
+  })
+
+  await knex.schema.createTable('recipes_ingredients', tbl => {
+    tbl.increments('id');
+    tbl.string('name').unique().notNullable();
+    tbl.integer('recipe_id').references('id').inTable('dishes').onDelete('CASCADE').onUpdate('CASCADE').notNullable();
+    tbl.integer('ingredient_id').references('id').inTable('dishes').onDelete('CASCADE').onUpdate('CASCADE').notNullable();
+    tbl.integer('ingredient_count').notNullable();
+  })
+};
+
+exports.down = async function(knex) {
+  await knex.schema.dropTableIfExists('dishes');
+  await knex.schema.dropTableIfExists('recipes');
+  await knex.schema.dropTableIfExists('ingredients');
+  await knex.schema.dropTableIfExists('recipes_ingredients');
+};
