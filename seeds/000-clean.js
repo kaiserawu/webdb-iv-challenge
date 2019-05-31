ba130f10d568b6d9cc6c1f1a9b0ad1e44924be4e
kaiserawu <kaiser.a.wu@gmail.com>
2019-05-30 19:06:11 -0700

    Made cleaner ignore knex migrations tables

--- seeds/000-clean.js
+++ seeds/000-clean.js
@@ -2,5 +2,11 @@ const cleaner = require('knex-cleaner');
 
 
 exports.seed = async function(knex) {
-  await cleaner.clean(knex);
+  await cleaner.clean(knex, {
+    mode: 'delete',
+    ignoreTables: [
+      'knex_migrations',
+      'knex_migrations_lock',
+    ]
+  });
 };
